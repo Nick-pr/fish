033c85b542a7f4fa0ab23eacf2211ecaa20052c7
Nick Raffaele <raffaele.nickp@gmail.com>
2024-02-15 20:35:45 -0800

    change prompt character

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -14,7 +14,7 @@ function fish_prompt --description 'Informative prompt'
         set -l statusb_color (set_color --bold $fish_color_status)
         set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-        printf '[%s] %s%s %s%s \n -> ' (prompt_login)  \
+        printf '[%s] %s%s %s%s \n 󰁔 ' (prompt_login)  \
             (set_color $fish_color_cwd) (prompt_pwd --dir-length=0) \
             (set_color normal)
     end
